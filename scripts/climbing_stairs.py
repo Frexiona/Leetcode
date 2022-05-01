119ece44194ea2d9cdfccbde830834587713334d
Haolin Zhang <frexiona@foxmail.com>
2022-05-01 20:11:10 +0100

    format fix

--- scripts/climbing_stairs.py
+++ scripts/climbing_stairs.py
@@ -1,7 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1: return 1
-        if n == 2: return 2
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
         # bottom up DP
         bottom_up = [0] * n
         bottom_up[0] = 1
